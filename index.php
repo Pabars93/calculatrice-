bde15c9ecf5602fe4c986fb43a91eeb60c2b29a8
Pabars93 <pabars.pa@gmail.com>
2025-03-23 20:26:50 +0100

    Modification

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Calculatrice en PHP</title>
+    <title>Calculatrice en PHP / Laravel</title>
 </head>
 <body>
     <h1>Calculatrice</h1>
